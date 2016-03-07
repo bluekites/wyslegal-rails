0e4185d8bf738bbc8b9bf9361ce0c24db41eccd9
Dean Chuang <azurenokite@gmail.com>
2016-03-07 07:01:02 +0000

    add CRUD for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,40 @@
 class PostsController < ApplicationController
   
+  before_action :set_up_list, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @posts = Post.all
+  end
+  
+  def show
+  end
+  
+  def new
     
   end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+    def set_up_post
+      @post = Post.find(params[:id])
+    end
+    
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
