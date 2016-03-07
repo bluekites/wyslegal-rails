1224509ca42937c23fd0f7df51a15d648e8132e6
Dean Chuang <azurenokite@gmail.com>
2016-03-07 07:39:56 +0000

    add friendly id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   
   private
     def set_up_post
-      @post = Post.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
     
     def post_params
