c9cbaf6af9be8c821359e6b9947d5f37fe4b3c13
Dean Chuang <azurenokite@gmail.com>
2016-03-17 20:56:51 +0000

    add admin route for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   root 'home#index'
   devise_for :users
+  devise_scope :user do
+    get "/admin" => "devise/sessions#new"
+  end
   resources :posts, path: "blog"
   resources :contacts, only: [:new, :create]
   get '*path' => redirect('/404')
