bf31179364851107bf5b944f1096b2c51dc87fd1
Dean Chuang <azurenokite@gmail.com>
2016-03-19 02:13:14 +0000

    testing user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   
   def contact_email(contact)
     @contact = contact
-    mail(to: ENV["OWNER_EMAIL"], from: @contact.email, subject: "Website Contact")
+    mail(to: "azurenokite@yahoo.com", from: @contact.email, subject: "Website Contact")
   end
 end
