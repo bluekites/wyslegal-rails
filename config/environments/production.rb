04341e12b3bac4ccf3ca7f9cdf813b134609bfdb
Dean Chuang <azurenokite@gmail.com>
2016-03-19 01:52:42 +0000

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,8 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { :host => 'https://wyslegal.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   
   # Prepend all log lines with the following tags.
