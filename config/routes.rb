25743314c000b5f1fb0968158d45d6d60afd7ecb
Dean Chuang <azurenokite@gmail.com>
2016-03-19 00:53:24 +0000

    update general styling for all forms and add edit user profile route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get "/admin" => "devise/sessions#new"
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration' 
   end
   resources :posts, path: "blog"
   resources :contacts, only: [:new, :create]
