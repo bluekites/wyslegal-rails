bb6bb0be63d02fe1f3a6b24e82e1db37d0a9fc1b
Dean Chuang <azurenokite@gmail.com>
2016-03-21 00:14:27 +0000

    add services controller/view and basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
   root 'home#index'
+  resources :posts, path: "blog"
+  resources :contacts, only: [:new, :create]
+  get '/services' => 'services#index'
   devise_for :users
   devise_scope :user do
-    get "/admin" => "devise/sessions#new"
+    get '/admin' => 'devise/sessions#new'
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
     put 'users' => 'devise/registrations#update', :as => 'user_registration' 
   end
-  resources :posts, path: "blog"
-  resources :contacts, only: [:new, :create]
+  
   get '*path' => redirect('/404')
   
 end
